6b6bfb13c0a16ea7391e32649efa1946a9967e70
Jose Miguel Fernandez C <174638@p.uapa.edu.do>
2020-06-16 22:17:55 -0300

    Commit inicial

---
+++ Hilos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System.Threading;
+
+namespace Hilos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(metodo));
+            t.Start();
+            for (int i = 0; i < 1000; i++)
+            
+                Console.Write("A");
+              
+              
+              
+                Console.Read();
+        }
+
+        static void metodo()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write("B");
+            }
+        }
+    }
+}
